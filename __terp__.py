c84bf518dc356cb2c7b209b0ef4f0ab9bf9539b7
izbaila <izbailaimtiaz17@gmail.com>
2015-05-05 12:45:48 +0500

    Initial structure of library

---
+++ __terp__.py
@@ -0,0 +1,13 @@
+{
+        'name':'cms_library_management',
+        'version':'0.1',
+        'category':'Tools',
+        'description':"""This module is for the library Management sysytem """,
+        'author':'izbaila and masood',
+        'website':'http://google.com/',
+        'depends':['base'],
+        'init_xml':[],
+        'update_xml':['cms_library_lms_view.xml'],
+        'demo_xml':[],
+        'installable': True,
+}
