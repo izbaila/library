a18d3067a9e643fc9ce95048517f4ee76aaa1d3c
Ghulam Masood <ghulam.masood@gmail.com>
2015-09-09 22:25:23 +0500

    Circulation reports formating completed

--- report/report_patron_library_history.py
+++ report/report_patron_library_history.py
@@ -1,6 +1,6 @@
 import pooler
 import time
-from datetime import date ,datetime 
+import datetime 
 import rml_parse
 from report import report_sxw
 import netsvc
@@ -15,11 +15,16 @@ class report_patron_library_history(rml_parse.rml_parse):
             self.localcontext.update({'get_issued_detail':self.get_issued_detail, 
                                       'get_borrower_detail':self.get_borrower_detail,
                                       'get_image':self.get_image,
+                                      'get_month':self.get_month,
                                    })
     def get_image(self,form):
         rec = pooler.get_pool(self.cr.dbname).get('lms.patron.registration').browse(self.cr,self.uid,form['borrower'])
         image = rec.student_id.image
         return image
+    
+    def get_month(self,form):
+        month = datetime.datetime.now().strftime("%m")
+        return month
     
     def get_borrower_detail(self,form):
             result = []
@@ -39,6 +44,7 @@ class report_patron_library_history(rml_parse.rml_parse):
         
     def get_issued_detail(self,form):
         result = []
+        sno = 0
         issued_resources_id = pooler.get_pool(self.cr.dbname).get('lms.issue').search(self.cr ,self.uid,[('borrower_id.id','=',form['borrower'])])
         j=0
         while j< len(issued_resources_id) :
@@ -46,20 +52,19 @@ class report_patron_library_history(rml_parse.rml_parse):
             ans = rec.resource
             if issued_resources_id:
                 for i in ans:
-                    my_dict = {'resource_no':'' ,'accession_no':'' ,'issue_no':'' ,'cataloge_date':'',
+                    my_dict = {'sno':'' ,'resource_no':'' ,'accession_no':'' ,'issue_no':'' ,'cataloge_date':'',
                                'return_date':'' ,'return_name':''}
+                    sno = sno + 1
+                    my_dict['sno'] = sno
                     my_dict['resource_no'] = i.resource_no.name
                     my_dict['accession_no'] = i.accession_no
                     my_dict['issue_no'] = rec.name
                     issue_date = mx.DateTime.strptime(rec.issue_date, '%Y-%m-%d').strftime("%h %d, %y ")
                     my_dict['cataloge_date'] = issue_date
                     r_id = pooler.get_pool(self.cr.dbname).get('lms.return').search(self.cr ,self.uid,[('returned_material.cataloge_id.id','=',i.id ),( 'borrower_id.id','=',form['borrower'])])
-                    #print "return ids=",r_id
                     for p in pooler.get_pool(self.cr.dbname).get('lms.return').browse(self.cr ,self.uid ,r_id):
                         d = p.return_date
                         return_date = mx.DateTime.strptime(d, '%Y-%m-%d').strftime("%h %d, %y ")
-                        #print mx.DateTime.strptime(d, '%Y-%m-%d ')
-                       
                         my_dict['return_date'] =return_date
                         my_dict['return_name'] = p.name
                     result.append(my_dict)
